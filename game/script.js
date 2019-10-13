e954ba0b723198806c7c4a51950f3074d96b66af
Lukasz Smolnicki <lukasz.smolnicki@gmail.com>
2019-10-13 19:50:52 +0200

    FZ12JF-12 map generator and player move added

---
+++ game/script.js
@@ -0,0 +1,70 @@
+let xPosition = 1;
+let yPosition = 1;
+
+function mapGenerator(mapWidth, mapHeight) {
+    let table = document.getElementById('map');
+    for (i = 0; i < mapHeight; i++) {
+        let row = document.createElement('tr');
+        for (let j = 0; j < mapWidth; j++) {
+            let col = document.createElement('td');
+            row.appendChild(col);
+            col.classList.add('x' + (j + 1));
+        }
+        table.appendChild(row);
+    }
+    for (i = 0; i < mapHeight; i++) {
+        let yHigh = table.rows[i]
+        for (j = 0; j < mapWidth; j++) {
+            yHigh.cells[j].classList.add('y' + (i + 1));
+        }
+    }
+    let elem = document.getElementById('btn');
+    elem.parentNode.removeChild(elem);
+}
+
+function playerPosition(xPosition, yPosition) {
+    movePlayer = document.querySelector(`.x${xPosition}.y${yPosition}`);
+    movePlayer.classList.add('player');
+    document.onkeydown = moveDirection;
+}
+
+function moveDirection(move) {
+    movePlayer.classList.remove('player');
+    if (move.keyCode == 39) {
+        xPosition = xPosition + 1;
+    } else if (move.keyCode == 37) {
+        xPosition = xPosition - 1;
+    } else if (move.keyCode == 40) {
+        yPosition = yPosition + 1;
+    } else if (move.keyCode == 38) {
+        yPosition = yPosition - 1;
+    }
+    playerPosition(xPosition, yPosition);
+}
+// switch (moveDirection) {
+//     case 'LEFT':
+//         nextPositionOfPlayer = $(this).find('td.car').prev();
+//         setupOfCar += 'car--left';
+//         break;
+//     case 'UP':
+//         nextPositionOfPlayer = $(this).find('td.car').parent().prev().find(':nth-child(' + (lastPositionOfCar.index() + 1) + ')');
+//         setupOfCar += 'car--up';
+//         break;
+//     case 'RIGHT':
+//         nextPositionOfPlayer = $(this).find('td.car').next();
+//         setupOfCar += 'car--right';
+//         break;
+//     case 'DOWN':
+//         nextPositionOfPlayer = $(this).find('td.car').parent().next().find(':nth-child(' + (lastPositionOfCar.index() + 1) + ')');
+//         setupOfCar += 'car--down';
+//         break;
+// }
+{/* <script>
+function anim(e) {
+    alert(e.keyCode);
+}
+if(e.keyCode==39){
+    
+}
+document.onkeydown = anim;    
+</script> */}
